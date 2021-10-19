ad2f97ee82089afe37981b9b37baba1e1f40a24e
varun1017 <86829344+varun1017@users.noreply.github.com>
2021-10-19 16:19:56 +0530

    Add files via upload

---
+++ floatmoodle1/floatmoodle1app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
+def save_profile(sender, instance, created, **kwargs):
+    user = instance
+    if created:
+        profile = Profile(user=user)
+        profile.save()
